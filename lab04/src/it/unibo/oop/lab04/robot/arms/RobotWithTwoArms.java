7891f70003b9c1a546e40448b70f020c89a414fd
mega2799 <megadigimatt@gmail.com>
2020-10-23 19:07:33 +0200

    robot is working [ O . O ]

--- lab04/src/it/unibo/oop/lab04/robot/arms/RobotWithTwoArms.java
+++ lab04/src/it/unibo/oop/lab04/robot/arms/RobotWithTwoArms.java
@@ -4,71 +4,56 @@ import it.unibo.oop.lab04.robot.base.BaseRobot;
 import it.unibo.oop.lab04.robot.base.Position2D;
 
 public class RobotWithTwoArms extends BaseRobot implements RobotWithArms{
-
+	
+    public static final double MOVEMENT_DELTA_CONSUMPTION_CARRY = 0.5;
+	private int itemsCarried;
+	
+	public final BasicArm[] arms  = new BasicArm[] {
+			new BasicArm("Right Arm"),
+			new BasicArm("Left Arm")
+	};
+	
 	public RobotWithTwoArms(String robotName) {
 		super(robotName);
-		// TODO Auto-generated constructor stub
+		this.itemsCarried = itemsCarried;
 	}
-
-	@Override
-	public boolean moveUp() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean moveDown() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean moveLeft() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean moveRight() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void recharge() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public double getBatteryLevel() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public Position2D getPosition() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
+	
 	public boolean pickUp() {
-		// TODO Auto-generated method stub
+		for(BasicArm b : arms) {
+			if (!b.isGrabbing() && isBatteryEnough(b.getPICK_UP_ENERGY())) { // if is not grabbing then 
+				b.pickUp();
+				this.itemsCarried++;
+				consumeBattery(b.getPICK_UP_ENERGY());
+				log(b + " is picking an object");
+				return b.isGrabbing();
+			}
+			log("Can not grab (battery=" + this.getBatteryLevel() + "," + b + " isGrabbing=" + b.isGrabbing() + ")");
+		}
+		
 		return false;
 	}
 
-	@Override
 	public boolean dropDown() {
-		// TODO Auto-generated method stub
+		for(BasicArm b : arms) {
+			if (b.isGrabbing() && isBatteryEnough(b.getDROP_DOWN_ENERGY())) {
+				b.dropDown();
+				this.itemsCarried--;
+				consumeBattery(b.getDROP_DOWN_ENERGY());
+				log(b + " is dropping an object");
+				return b.isGrabbing();
+			}
+			log("Can not drop (battery=" + this.getBatteryLevel() + "," + b + " isGrabbing=" + b.isGrabbing() + ")");
+		}
 		return false;
 	}
 
-	@Override
 	public int getItemsCarried() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.itemsCarried;
 	}
-
+	
+	protected double getBatteryRequirementForMovement() {
+       return super.getBatteryRequirementForMovement() + MOVEMENT_DELTA_CONSUMPTION_CARRY ;
+    }
+	
 	
 }
